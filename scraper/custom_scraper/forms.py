2c7a79c38c2ffd553bf7982f3f00504e151b9015
Juan Manuel <jmdedio@gmail.com>
2017-08-10 11:34:37 -0300

    10/08/2017 1

--- scraper/custom_scraper/forms.py
+++ scraper/custom_scraper/forms.py
@@ -4,8 +4,8 @@ from django import forms
 from django.forms.extras import SelectDateWidget
 
 class Prueba(forms.Form):
-    start_date = forms.DateField(widget=SelectDateWidget())
-    end_date = forms.DateField(widget=SelectDateWidget())
+    start_date = forms.DateField(widget=SelectDateWidget(attrs={'class':'selmenu',}))
+    end_date = forms.DateField(widget=SelectDateWidget(attrs={'class':'selmenu',}))
 
     def __init__(self, *args, **kwargs):
         super(Prueba, self).__init__(*args, **kwargs)
